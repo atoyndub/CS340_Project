c2bbba5e09763b7ac709f921957c1f795f47fa96
atoyndub <atoyndub@gmail.com>
2021-02-08 08:19:05 -0800

    finished making all the add pages except that for photos

--- add_event_load.js
+++ add_event_load.js
@@ -1,4 +1,4 @@
 //loadSelectorList(toggleID, selectorListID, selectorListBodyID, destinationListBodyID)
-loadSelectorList("PersonSelectorToggle", "PersonSelector", "SelectPersonList", "AddEventPersonsList");
-loadSelectorList("PlaceSelectorToggle", "PlaceSelector", "SelectPlacesList", "AddEventPlacesList");
-loadSelectorList("PhotoSelectorToggle", "PhotoSelector", "SelectPhotosList", "AddEventPhotosList");
+loadSelectorList("AddEvent_PersonToggle", "AddEvent_PersonSelector", "AddEvent_PersonSelectorList", "AddEvent_PersonLinkList");
+loadSelectorList("AddEvent_PlaceToggle", "AddEvent_PlaceSelector", "AddEvent_PlaceSelectorList", "AddEvent_PlaceLinkList");
+loadSelectorList("AddEvent_PhotoToggle", "AddEvent_PhotoSelector", "AddEvent_PhotoSelectorList", "AddEvent_PhotoLinkList");
---
+++ add_person_load.js
@@ -0,0 +1,7 @@
+//loadSelectorList(toggleID, selectorListID, selectorListBodyID, destinationListBodyID)
+loadSelectorList("AddPerson_ParentToggle", "AddPerson_ParentSelector", "AddPerson_ParentSelectorList", "AddPerson_ParentLinkList");
+loadSelectorList("AddPerson_SpouseToggle", "AddPerson_SpouseSelector", "AddPerson_SpouseSelectorList", "AddPerson_SpouseLinkList");
+loadSelectorList("AddPerson_ChildToggle", "AddPerson_ChildSelector", "AddPerson_ChildSelectorList", "AddPerson_ChildLinkList");
+loadSelectorList("AddPerson_PlaceToggle", "AddPerson_PlaceSelector", "AddPerson_PlaceSelectorList", "AddPerson_PlaceLinkList");
+loadSelectorList("AddPerson_EventToggle", "AddPerson_EventSelector", "AddPerson_EventSelectorList", "AddPerson_EventLinkList");
+loadSelectorList("AddPerson_PhotoToggle", "AddPerson_PhotoSelector", "AddPerson_PhotoSelectorList", "AddPerson_PhotoLinkList");
