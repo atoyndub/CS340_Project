829dcf8de65957b3ccb4c047f5c42b6d78a80517
atoyndub <atoyndub@gmail.com>
2021-02-16 17:18:00 -0800

    removed places list from update event page

--- update_event_load.js
+++ update_event_load.js
@@ -1,7 +1,5 @@
 //loadSelectorList(toggleID, selectorListID, selectorListBodyID, destinationListBodyID)
 loadSelectorList("UpdateEvent_PersonToggle", "UpdateEvent_PersonSelector", "UpdateEvent_PersonSelectorList", "UpdateEvent_PersonLinkList");
 addRemoveOptionToSelectionListElements("UpdateEvent_PersonLinkList");
-loadSelectorList("UpdateEvent_PlaceToggle", "UpdateEvent_PlaceSelector", "UpdateEvent_PlaceSelectorList", "UpdateEvent_PlaceLinkList");
-addRemoveOptionToSelectionListElements("UpdateEvent_PlaceLinkList");
 loadSelectorList("UpdateEvent_PhotoToggle", "UpdateEvent_PhotoSelector", "UpdateEvent_PhotoSelectorList", "UpdateEvent_PhotoLinkList");
 addRemoveOptionToSelectionListElements("UpdateEvent_PhotoLinkList");
